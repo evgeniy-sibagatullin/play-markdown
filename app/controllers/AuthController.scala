fcf323731d36a57cef154605452149a685163c5f
evgeniy-sibagatullin <evgeniy.sibagatullin@gmail.com>
2015-10-27 19:02:57 +0200

    United all Silhouette controllers to single AuthController.

---
+++ app/controllers/AuthController.scala
@@ -0,0 +1,168 @@
+package controllers
+
+import java.util.UUID
+import javax.inject.Inject
+
+import com.mohiva.play.silhouette.api.Authenticator.Implicits._
+import com.mohiva.play.silhouette.api._
+import com.mohiva.play.silhouette.api.exceptions.ProviderException
+import com.mohiva.play.silhouette.api.repositories.AuthInfoRepository
+import com.mohiva.play.silhouette.api.services.AvatarService
+import com.mohiva.play.silhouette.api.util.{Clock, Credentials, PasswordHasher}
+import com.mohiva.play.silhouette.impl.authenticators.CookieAuthenticator
+import com.mohiva.play.silhouette.impl.exceptions.IdentityNotFoundException
+import com.mohiva.play.silhouette.impl.providers.{SocialProviderRegistry, _}
+import forms.{SignInForm, _}
+import models.User
+import models.services.UserService
+import net.ceedubs.ficus.Ficus._
+import play.api.Configuration
+import play.api.i18n.{Messages, MessagesApi}
+import play.api.libs.concurrent.Execution.Implicits._
+import play.api.mvc.Action
+
+import scala.concurrent.Future
+import scala.concurrent.duration.FiniteDuration
+import scala.language.postfixOps
+
+
+/**
+ * The basic application controller.
+ *
+ * @param messagesApi The Play messages API.
+ * @param env The Silhouette environment.
+ * @param userService The user service implementation.
+ * @param authInfoRepository The auth info repository implementation.
+ * @param credentialsProvider The credentials provider.
+ * @param socialProviderRegistry The social provider registry.
+ * @param configuration The Play configuration.
+ * @param clock The clock instance.
+ */
+class AuthController @Inject() (
+  val messagesApi: MessagesApi,
+  val env: Environment[User, CookieAuthenticator],
+  socialProviderRegistry: SocialProviderRegistry,
+  userService: UserService,
+  authInfoRepository: AuthInfoRepository,
+  credentialsProvider: CredentialsProvider,
+  configuration: Configuration,
+  clock: Clock,
+  avatarService: AvatarService,
+  passwordHasher: PasswordHasher)
+  extends Silhouette[User, CookieAuthenticator] {
+
+  /**
+   * Handles the Sign In action.
+   *
+   * @return The result to display.
+   */
+  def signIn = UserAwareAction.async { implicit request =>
+    request.identity match {
+      case Some(user) => Future.successful(Redirect(routes.MarkdownController.index()))
+      case None => Future.successful(Ok(views.html.signIn(SignInForm.form, socialProviderRegistry)))
+    }
+  }
+
+  /**
+   * Handles the Sign Out action.
+   *
+   * @return The result to display.
+   */
+  def signOut = SecuredAction.async { implicit request =>
+    val result = Redirect(routes.MarkdownController.index())
+    env.eventBus.publish(LogoutEvent(request.identity, request, request2Messages))
+    env.authenticatorService.discard(request.authenticator, result)
+  }
+
+  /**
+    * Handles the Sign Up action.
+   *
+   * @return The result to display.
+   */
+  def signUp = UserAwareAction.async { implicit request =>
+    request.identity match {
+      case Some(user) => Future.successful(Redirect(routes.MarkdownController.index()))
+      case None => Future.successful(Ok(views.html.signUp(SignUpForm.form)))
+    }
+  }
+
+  /**
+   * Authenticates a user against the credentials provider.
+   *
+   * @return The result to display.
+   */
+  def authenticate = Action.async { implicit request =>
+    SignInForm.form.bindFromRequest.fold(
+      form => Future.successful(BadRequest(views.html.signIn(form, socialProviderRegistry))),
+      data => {
+        val credentials = Credentials(data.email, data.password)
+        credentialsProvider.authenticate(credentials).flatMap { loginInfo =>
+          val result = Redirect(routes.MarkdownController.index())
+          userService.retrieve(loginInfo).flatMap {
+            case Some(user) =>
+              val c = configuration.underlying
+              env.authenticatorService.create(loginInfo).map {
+                case authenticator if data.rememberMe =>
+                  authenticator.copy(
+                    expirationDateTime = clock.now + c.as[FiniteDuration]("silhouette.authenticator.rememberMe.authenticatorExpiry"),
+                    idleTimeout = c.getAs[FiniteDuration]("silhouette.authenticator.rememberMe.authenticatorIdleTimeout"),
+                    cookieMaxAge = c.getAs[FiniteDuration]("silhouette.authenticator.rememberMe.cookieMaxAge")
+                  )
+                case authenticator => authenticator
+              }.flatMap { authenticator =>
+                env.eventBus.publish(LoginEvent(user, request, request2Messages))
+                env.authenticatorService.init(authenticator).flatMap { v =>
+                  env.authenticatorService.embed(v, result)
+                }
+              }
+            case None => Future.failed(new IdentityNotFoundException("Couldn't find user"))
+          }
+        }.recover {
+          case e: ProviderException =>
+            Redirect(routes.AuthController.signIn()).flashing("error" -> Messages("error.invalid.credentials"))
+        }
+      }
+    )
+  }
+
+  /**
+   * Registers a new user.
+   *
+   * @return The result to display.
+   */
+  def signUpPost = Action.async { implicit request =>
+    SignUpForm.form.bindFromRequest.fold(
+      form => Future.successful(BadRequest(views.html.signUp(form))),
+      data => {
+        val loginInfo = LoginInfo(CredentialsProvider.ID, data.email)
+        userService.retrieve(loginInfo).flatMap {
+          case Some(user) =>
+            Future.successful(Redirect(routes.AuthController.signUp()).flashing("error" -> Messages("error.user.exists")))
+          case None =>
+            val authInfo = passwordHasher.hash(data.password)
+            val user = User(
+              userID = UUID.randomUUID(),
+              loginInfo = loginInfo,
+              firstName = Some(data.firstName),
+              lastName = Some(data.lastName),
+              fullName = Some(data.firstName + " " + data.lastName),
+              email = Some(data.email),
+              avatarURL = None
+            )
+            for {
+              avatar <- avatarService.retrieveURL(data.email)
+              user <- userService.save(user.copy(avatarURL = avatar))
+              authInfo <- authInfoRepository.add(loginInfo, authInfo)
+              authenticator <- env.authenticatorService.create(loginInfo)
+              value <- env.authenticatorService.init(authenticator)
+              result <- env.authenticatorService.embed(value, Redirect(routes.MarkdownController.index()))
+            } yield {
+              env.eventBus.publish(SignUpEvent(user, request, request2Messages))
+              env.eventBus.publish(LoginEvent(user, request, request2Messages))
+              result
+            }
+        }
+      }
+    )
+  }
+}
