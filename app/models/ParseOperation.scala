38c600aea4392e832fe66c46f94b1e325415fc02
evgeniy-sibagatullin <evgeniy.sibagatullin@gmail.com>
2015-10-27 18:35:56 +0200

    Renamed ParseOperation entity.

--- app/models/ParseOperation.scala
+++ app/models/ParseOperation.scala
@@ -10,7 +10,7 @@ import org.joda.time.DateTime
  * @param resultHtml Successfully generated HTML.
  * @param creationDateTime Surprisingly creation date and time.
  */
-case class ParseOperationInfo (
+case class ParseOperation (
   id: String,
   email: String,
   textToParse: String,
@@ -21,12 +21,12 @@ case class ParseOperationInfo (
 /**
  * The companion object.
  */
-object ParseOperationInfo {
+object ParseOperation {
   import play.api.libs.json._
 
   /**
    * Converts the [ParseOperationInfo] object to Json and vice versa.
    */
-  implicit val jsonFormat = Json.format[ParseOperationInfo]
+  implicit val jsonFormat = Json.format[ParseOperation]
 
 }
